1c65e7d4e32d376124b64314b269b45da683c17a
Aikis07 <danila.barkov@gmail.com>
2017-11-16 20:27:54 +0300

    test_01

---
+++ src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        int first = 5;
+        int second = 5;
+
+        int sum = first * second;
+        System.out.println(sum);
+    }
+}
